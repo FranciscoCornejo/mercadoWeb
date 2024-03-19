db25f35d08cc27340056e22c2eb63c58edbf0590
O-marin <omarancb@gmail.com>
2024-03-19 13:48:19 -0300

    se agrega main.hbs a layouts, dashboard y nav hbs a partials y una view products.hbs todo:corregir el dashboard para que queden horizontal en filas

--- script.js
+++ script.js
@@ -1,2 +1,27 @@
 const express = require('express');
 const app = express();
+const exphbs= require('express-handlebars')
+const port = 3000;
+
+//motor de plantillas handlebars
+app.set('view engine', 'hbs');
+app.engine('hbs', exphbs.engine({
+    defaultLayour:'main',
+    layoutsDir: __dirname + '/views/layouts',
+    partialsDir: __dirname + '/views/partials',
+    extname: 'hbs' 
+}))
+
+app.use(express.static('assets'));
+app.use('/bootstrap',express.static('node_modules/bootstrap/dist/'))
+
+app.get('/products', (req,res)=>{
+    res.render('products', {
+        title:'Productos',
+        producto: ['banana','cebollas','lechuga','papas','pimenton','tomate']
+    })
+})
+
+
+
+app.listen(port,()=>console.log(`Server running on port ${port}`))
